54aa696c5500270c71d5b79b4e4a3c57c68f6ba9
Peeradont48 <peeradonte@gmail.com>
2024-07-22 13:36:22 +0700

    Import asstes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
+// CSS use BEM method -> Block Element Modifier
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 }

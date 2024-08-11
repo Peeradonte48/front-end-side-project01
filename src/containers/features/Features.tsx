f0c73388bdb8948629bea3a465c2a5a16c64aee0
Peeradont48 <peeradonte@gmail.com>
2024-08-11 17:51:54 +0700

    s

--- src/containers/features/Features.tsx
+++ src/containers/features/Features.tsx
@@ -3,12 +3,13 @@ import "./features.css";
 
 export default function Features() {
   return (
-    <div className="gpt3__features-container__feature">
-      {/* <div className="gpt3__features-container__feature-title">
-        <div />
-        <h1>{title}</h1>
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradiant-text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
       </div>
-      <div className="gpt3__features-container__feature-text">{text}</div> */}
     </div>
   );
 }

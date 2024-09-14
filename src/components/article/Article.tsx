12e93066780e417df0c68527fc16ced71a8ef6cd
Peeradont48 <peeradonte@gmail.com>
2024-09-14 15:43:12 +0700

    Add blog section

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 import "./article.css";
-export default function Article() {
-  return <div>Article</div>;
+
+interface articleProp {
+  imgUrl: string;
+  date: string;
+  title: string;
+}
+
+export default function Article({ imgUrl, date, title }: articleProp) {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
